91e3484bfadf912d149ff3957ab01049f41f79c7
Sayali2005894 <sayalinavkar@gmail.com>
2026-05-11 12:18:27 +0530

    ask  any enquiry

---
+++ src/main/java/com/example/enquiry_management/EnquiryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.enquiry_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnquiryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnquiryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/enquiry_management/EnquiryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enquiry_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnquiryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
